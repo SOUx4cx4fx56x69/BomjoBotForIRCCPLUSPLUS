3c858d041b092f18e69aea2a005e7cce96989f05
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-05-17 23:19:55 +0700

    .

--- BotFunctions.hpp
+++ BotFunctions.hpp
@@ -5,6 +5,7 @@ class BotFunctions : private Bot
 private:
  int self_socket;
  bool MessageUnderstanding(char*msg);
+ static char ** Self_Commands;
 public:
  BotFunctions(int socket);
  bool BotCommand(char*nick,char*realname,char*adress,char*TypeMessage,char*channel,char*command);
